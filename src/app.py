291e0f4ddf9f319f78958f8f8bd1b51751a550e2
Javier Esaú Castillo Villalobos <21000643@alumnos.utleon.edu.mx>
2024-10-30 20:54:18 -0600

    Insertar

--- src/app.py
+++ src/app.py
@@ -13,15 +13,59 @@ con=MySQL(app)
 def lista_alumnos():
     try:
         cursor=con.connection.cursor()
-        sql='select * from alumnos'
+        sql='select * from alumnos order by nombre ASC'
         cursor.execute(sql)
         datos=cursor.fetchall()
         alumnos=[]
         for fila in datos:
-            alumno={ 'matricula': fila[0], 'nombre':[1], 'apaterno':[2], 'amaterno':[3], 'correo':[4] }
+            alumno={ 'matricula': fila[0], 'nombre':fila[1], 'apaterno':fila[2], 'amaterno':fila[3], 'correo':fila[4] }
             alumnos.append(alumno)
             print(alumnos)
         return jsonify({'alumnos': alumnos, 'mensaje': 'Lista de Alumnos', 'exito':True})
+    except Exception as ex:
+        return jsonify({"message": "Error al conectar a la base de datos {}".format(ex), 'exito':False})
+
+
+def leer_alumnos_bd(matricula):
+    try:
+        cursor=con.connection.cursor()
+        sql='select * from alumnos where matricula={0}'.format(matricula)
+        cursor.execute(sql)
+        datos=cursor.fetchone()
+        if datos!=None:
+            alumno={'matricula': datos[0], 'nombre':datos[1], 'apaterno':datos[2], 'amaterno':datos[3], 'correo':datos[4] }
+            return alumno
+        else:
+            return None
+    except Exception as ex:
+        return jsonify({"message": "Error al conectar a la base de datos {}"})
+
+@app.route("/alumnos/<mat>", methods=['GET'])
+def leer_alumnos(mat):
+    try:
+        alumno=leer_alumnos_bd(mat)
+        if alumno!=None:
+            return jsonify({'alumnos': alumno, 'mensaje': 'Alumno encontrado', 'exito':True})  
+        else:
+            return jsonify({"mensaje": "Alumno no encontrado", 'exito':False})
+    except Exception as ex:
+        return jsonify({"message": "Error al conectar a la base de datos {}".format(ex), 'exito':False})
+
+@app.route("/alumnos", methods=['POST'])
+def registrar_alumno():
+    try:
+        alumno=leer_alumnos_bd(request.json['matricula'])
+        if alumno!=None:
+            return jsonify({'mensaje': 'Alumno ya existe', 'exito': False})
+        else:
+            cursor=con.connection.cursor()
+            sql='''insert into alumnos (matricula, nombre, apaterno, amaterno, correo)
+            values('{0}', '{1}', '{2}', '{3}', '{4}')'''.format(
+                request.json['matricula'], request.json['nombre'], request.json['apaterno'], request.json['amaterno'], 
+                request.json['correo'])
+            cursor.execute(sql)
+            con.connection.commit()
+            return jsonify({'message': 'Alumno Agregado', 'exito':True})
     except Exception as ex:
         return jsonify({"message": "Error al conectar a la base de datos {}".format(ex), 'exito':False})
 
